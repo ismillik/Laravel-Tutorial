67ed0d695189854f9ee7258b8da8a5df9c120d71
Ian Milliken <ianmilliken1215@gmail.com>
2022-04-11 10:50:53 -0400

    through lesson 12

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,22 +5,18 @@
 
 
 <body>
-    <article>
-        <h1><a href='/posts/my-first-post'>My First Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At elementum eu facilisis sed odio. Elementum facilisis leo vel fringilla est ullamcorper. Viverra maecenas accumsan lacus vel facilisis volutpat. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Non odio euismod lacinia at quis risus sed. Aliquam faucibus purus in massa tempor. Condimentum id venenatis a condimentum vitae. Elit sed vulputate mi sit amet mauris. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Tempus imperdiet nulla malesuada pellentesque elit. Tincidunt vitae semper quis lectus nulla at volutpat. Orci eu lobortis elementum nibh tellus molestie nunc. Purus sit amet luctus venenatis lectus magna. Enim nulla aliquet porttitor lacus luctus accumsan tortor. Eu scelerisque felis imperdiet proin fermentum leo.
-        </p>
-    </article>
-    <article>
-        <h1><a href='/posts/my-second-post'>My Second Post</a></h1>        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At elementum eu facilisis sed odio. Elementum facilisis leo vel fringilla est ullamcorper. Viverra maecenas accumsan lacus vel facilisis volutpat. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Non odio euismod lacinia at quis risus sed. Aliquam faucibus purus in massa tempor. Condimentum id venenatis a condimentum vitae. Elit sed vulputate mi sit amet mauris. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Tempus imperdiet nulla malesuada pellentesque elit. Tincidunt vitae semper quis lectus nulla at volutpat. Orci eu lobortis elementum nibh tellus molestie nunc. Purus sit amet luctus venenatis lectus magna. Enim nulla aliquet porttitor lacus luctus accumsan tortor. Eu scelerisque felis imperdiet proin fermentum leo.
-        </p>
-    </article>
-    <article>
-        <h1><a href='/posts/my-third-post'>My Third Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At elementum eu facilisis sed odio. Elementum facilisis leo vel fringilla est ullamcorper. Viverra maecenas accumsan lacus vel facilisis volutpat. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Non odio euismod lacinia at quis risus sed. Aliquam faucibus purus in massa tempor. Condimentum id venenatis a condimentum vitae. Elit sed vulputate mi sit amet mauris. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Tempus imperdiet nulla malesuada pellentesque elit. Tincidunt vitae semper quis lectus nulla at volutpat. Orci eu lobortis elementum nibh tellus molestie nunc. Purus sit amet luctus venenatis lectus magna. Enim nulla aliquet porttitor lacus luctus accumsan tortor. Eu scelerisque felis imperdiet proin fermentum leo.
-        </p>
-    </article>
+
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <h1>
+                <a href= '/posts/<?= $post->slug; ?>'>
+                    <?= $post->title; ?>
+                </a>
+            </h1>
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
+        </article>
+    <?php endforeach; ?>
     
 </body>
